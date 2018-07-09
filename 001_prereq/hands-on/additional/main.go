1278bc4ea42e3f4f0a074d2b3ab07277c7ab2232
James Lucktaylor <jlucktay@gmail.com>
2018-07-09 20:20:01 +0100

    Finished exercises up to ten

--- 001_prereq/hands-on/additional/main.go
+++ 001_prereq/hands-on/additional/main.go
@@ -5,6 +5,12 @@ import "fmt"
 func main() {
 	one()
 	two()
+	four()
+	five()
+	six()
+	eight()
+	nine()
+	ten()
 }
 
 func one() {
@@ -38,4 +44,111 @@ func two() {
 	for a, b := range m {
 		fmt.Printf("%v = %v\n", a, b)
 	}
+}
+
+type person struct {
+	fName, lName string
+}
+
+func four() {
+	p1 := person{"Firstname", "Lastname"}
+
+	fmt.Println(p1)
+	fmt.Println(p1.fName)
+}
+
+type personFive struct {
+	person
+	favFood []string
+}
+
+func five() {
+	p1 := personFive{
+		person{
+			"Firstname",
+			"Lastname",
+		},
+		[]string{
+			"pizza",
+			"dumplings",
+			"rice",
+		},
+	}
+
+	fmt.Println(p1.favFood)
+
+	for f, g := range p1.favFood {
+		fmt.Printf("food '%d' = '%s'\n", f, g)
+	}
+}
+
+type personSix struct {
+	personFive
+}
+
+func (ps personSix) walk() string {
+	return fmt.Sprintf("%s is walking.", ps.fName)
+}
+
+func six() {
+	s := personSix{personFive{person{"Firstname", "Lastname"}, []string{"apples", "oranges"}}}.walk()
+
+	fmt.Println(s)
+}
+
+type vehicle struct {
+	doors int
+	color string
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+func eight() {
+	t := truck{vehicle{2, "red"}, true}
+	s := sedan{vehicle{4, "blue"}, true}
+
+	fmt.Println(t)
+	fmt.Println(s)
+	fmt.Println(t.color)
+	fmt.Println(s.luxury)
+}
+
+func (t truck) transportationDevice() string {
+	return fmt.Sprintf("I am a %s truck with %d doors. Do I have four-wheel drive? %v", t.color, t.doors, t.fourWheel)
+}
+
+func (s sedan) transportationDevice() string {
+	return fmt.Sprintf("I am a %s sedan with %d doors. Am I a luxury model? %v", s.color, s.doors, s.luxury)
+}
+
+func nine() {
+	t := truck{vehicle{2, "red"}, true}
+	s := sedan{vehicle{4, "blue"}, true}
+
+	fmt.Println(t.transportationDevice())
+	fmt.Println(s.transportationDevice())
+}
+
+type transportation interface {
+	transportationDevice() string
+}
+
+func report(t transportation) {
+	fmt.Println(t.transportationDevice())
+}
+
+func ten() {
+	t := truck{vehicle{2, "red"}, true}
+	s := sedan{vehicle{4, "blue"}, true}
+
+	report(t)
+	report(s)
 }
