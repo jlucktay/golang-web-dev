d331aff1e747ea6adfc1580af06954d9e1ddaff8
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-26 11:00:07 +0100

    Implement 1) GET with dynamic responses to paths, and 2) HEAD

---
+++ 016_building-a-tcp-server-for-http/04_hands-on/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	method, path := "", ""
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			method = strings.Fields(ln)[0]
+			path = strings.Fields(ln)[1]
+			i++
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+	}
+
+	// write response
+	switch method {
+	case "GET":
+		respondGet(conn, path)
+	case "HEAD":
+		respondHead(conn)
+	default:
+		respondUnknown(conn)
+	}
+}
+
+func respondHead(conn net.Conn) {
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+}
+
+func respondGet(conn net.Conn, path string) {
+	bodyTop := `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Hello World</title>
+</head>
+
+<body>
+	<strong>`
+
+	bodyBottom := `</strong>
+</body>
+
+</html>`
+
+	body := bodyTop + path + bodyBottom
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func respondUnknown(conn net.Conn) {
+	fmt.Fprint(conn, "HTTP/1.1 400 Bad Request\r\n")
+}
