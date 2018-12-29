02570e255acf7d506bf85d5a8105ba741e0ba1ba
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-29 20:33:47 +0100

    Duplicate 022/01/03 to start 022/01/05 with

---
+++ 022_hands-on/01/05_hands-on/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/me", me)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "index!")
+}
+
+type person struct {
+	Name string
+}
+
+type parseMe struct {
+	People []person
+	Doggy  string
+	Now    time.Time
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	data := parseMe{
+		People: []person{
+			{"Evička"},
+			{"Karolinka"},
+			{"Katka"},
+			{"Eva"},
+			{"Pawel"},
+			{"Jimmy"},
+		},
+		Doggy: "Rex",
+		Now:   time.Now(),
+	}
+	tpl := template.Must(template.ParseFiles("tpl.gohtml"))
+
+	err := tpl.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "it'sa me, Jimmy!")
+}
