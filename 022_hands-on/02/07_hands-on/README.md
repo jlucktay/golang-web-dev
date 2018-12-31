379274c873fe7809fc75ac872b77d44ef03fe1e0
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-31 06:54:19 +0000

    Start 022/02/07

--- 022_hands-on/02/07_hands-on/README.md
+++ 022_hands-on/02/07_hands-on/README.md
@@ -1,4 +1,4 @@
-# Building upon the code from the previous problem:
+# Building upon the code from the previous problem
 
 Extract the code you wrote to READ from the connection using bufio.NewScanner into its own function called "serve".
 
---
+++ 022_hands-on/02/07_hands-on/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	l, errL := net.Listen("tcp", ":8080")
+	if errL != nil {
+		log.Fatal(errL)
+	}
+	defer l.Close()
+
+	for {
+		c, errC := l.Accept()
+		if errC != nil {
+			log.Fatal(errC)
+			continue
+		}
+
+		s := bufio.NewScanner(c)
+		for s.Scan() {
+			ln := s.Text()
+			fmt.Println(ln)
+
+			if ln == "" {
+				break
+			}
+		}
+
+		fmt.Println("Code got here.")
+		io.WriteString(c, fmt.Sprintf("I see you connected from '%v' at '%v'.\r\n", c.RemoteAddr(), time.Now()))
+
+		c.Close()
+	}
+}
