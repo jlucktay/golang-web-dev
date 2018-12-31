7a7868c84d475e56dd20d22033728e745ca28cb3
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-31 07:23:13 +0000

    Complete 022/02/11

--- 022_hands-on/02/11_hands-on/main.go
+++ 022_hands-on/02/11_hands-on/main.go
@@ -33,13 +33,21 @@ func serve(c net.Conn) {
 	s := bufio.NewScanner(c)
 	for s.Scan() {
 		ln := s.Text()
-		fmt.Println(ln)
 
 		if ln == "" {
 			fmt.Println("End of HTTP headers")
 			break
 		}
+
+		fmt.Println(ln)
 	}
 
-	io.WriteString(c, fmt.Sprintf("I see you connected from '%v' at '%v'.\r\n", c.RemoteAddr(), time.Now()))
+	body := fmt.Sprintf("I see you connected from address '%v' at timestamp '%v'.\r\n",
+		c.RemoteAddr(), time.Now())
+
+	io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(c, "Content-Type: text/plain\r\n")
+	io.WriteString(c, "\r\n")
+	io.WriteString(c, body)
 }
