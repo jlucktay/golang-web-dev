db14228290424f0597c86b2216ff950b03ba6380
James Lucktaylor <jlucktay+github@gmail.com>
2019-01-03 10:22:57 +0000

    WIP 022/02/17 - implement GET /

--- 022_hands-on/02/17_hands-on/main.go
+++ 022_hands-on/02/17_hands-on/main.go
@@ -1,3 +1,8 @@
+// Add code to respond to the following METHODS & ROUTES:
+// - GET /
+// - GET /apply
+// - POST /apply
+
 package main
 
 import (
@@ -29,38 +34,54 @@ func main() {
 
 func serve(c net.Conn) {
 	defer c.Close()
+
 	firstLine := true
 	var rMethod, rTarget string
 	s := bufio.NewScanner(c)
+
 	for s.Scan() {
 		ln := s.Text()
 		fmt.Println(ln)
+
 		if firstLine {
 			xs := strings.Fields(ln)
 			rMethod, rTarget = xs[0], xs[1]
 			fmt.Println("Method:", rMethod)
 			fmt.Println("Request target:", rTarget)
 		}
+
 		if ln == "" {
 			fmt.Println("End of HTTP headers")
 			break
 		}
+
 		firstLine = false
 	}
 
+	switch {
+	case rMethod == "GET" && rTarget == "/":
+		getRoot(c)
+	case rMethod == "GET" && rTarget == "/favicon.ico":
+		// empty branch to stop the server from crashing out
+	default:
+		log.Fatalf("unknown verb '%v' or path '%v'", rMethod, rTarget)
+	}
+}
+
+func getRoot(c net.Conn) {
 	body := `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-`
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+	`
 	body += "<h1>HOLY COW THIS IS LOW LEVEL</h1>\n"
 	body += fmt.Sprintf("I see you connected from address <strong>'%v'</strong> at timestamp <strong>'%v'</strong>.<br />\n",
 		c.RemoteAddr(), time.Now())
-	body += fmt.Sprintf("Method: %v<br />\n", rMethod)
-	body += fmt.Sprintf("Request target: %v<br />\n", rTarget)
+	body += "Method: GET<br />\n"
+	body += "Request target: /<br />\n"
 	body += "</body>\n</html>"
 
 	io.WriteString(c, "HTTP/1.1 200 OK\r\n")
@@ -69,3 +90,6 @@ func serve(c net.Conn) {
 	io.WriteString(c, "\r\n")
 	io.WriteString(c, body)
 }
+
+func getApply()  {}
+func postApply() {}
