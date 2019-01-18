8b4e5990e8b5689f716ac0aba0db717f4fa3bfc6
James Lucktaylor <jlucktay+github@gmail.com>
2019-01-18 13:02:48 +0000

    WIP script; install gcc as well, and retry go build

--- 033_aws-scaling/04_hands-on/01_challenge/tf/gce.startup.sh
+++ 033_aws-scaling/04_hands-on/01_challenge/tf/gce.startup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function log(){
-    echo "[$(date '+%Y%m%d.%H%M%S.%N%z')] $1" >> /home/jameslucktaylor/gce.startup.log
+    echo "[$(date '+%Y%m%d.%H%M%S.%N%z')] $1" | sudo -u jameslucktaylor tee --append /home/jameslucktaylor/gce.startup.log
 }
 
 # Timestamp start
@@ -12,11 +12,11 @@ log "cloud-init: start"
 # Drop a note when this script is done (note: 'done' might include exiting prematurely due to an error!)
 trap "log 'cloud-init: finish'" INT TERM EXIT
 
-log "Catting '.toprc'..."
-sudo -u jameslucktaylor tee /home/jameslucktaylor/.toprc <<'EOF'
-${toprc}
-EOF
-log "Catted '.toprc'."
+# log "Catting '.toprc'..."
+# sudo -u jameslucktaylor tee /home/jameslucktaylor/.toprc <<'EOF'
+# ${toprc}
+# EOF
+# log "Catted '.toprc'."
 
 log "Running 'apt'..."
 # Run patches and install golang
@@ -26,6 +26,8 @@ log "'apt upgrade'..."
 apt upgrade --assume-yes --no-install-recommends
 log "'apt install golang-go'..."
 apt install golang-go --assume-yes --no-install-recommends
+log "'apt install gcc'..."
+apt install gcc --assume-yes --no-install-recommends
 log "'apt autoremove'..."
 apt autoremove --assume-yes
 log "Finished 'apt'."
@@ -34,7 +36,9 @@ log "Fetching main.go from GitHub..."
 curl https://raw.githubusercontent.com/jlucktay/golang-web-dev/master/033_aws-scaling/04_hands-on/01_challenge/tf/go/main.go | sudo -u jameslucktaylor tee /home/jameslucktaylor/main.go
 log "Fetched main.go from GitHub."
 
-# log "Building 'revolver' binary..."
-# go build -o revolver -a -ldflags '-extldflags "-static"' .
+log "Building 'revolver' binary..."
+go build -o revolver -a -ldflags '-extldflags "-static"' main.go
+log "Built 'revolver' binary."
+
 # # echo "Hello, World" > index.html
 # # nohup busybox httpd -f -p 8080 &
