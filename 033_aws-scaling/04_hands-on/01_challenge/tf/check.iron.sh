cd0d8ee7643914ff1d1932442f721c1bd8de3614
James Lucktaylor <jlucktay+github@gmail.com>
2019-01-19 20:04:26 +0000

    Add scripts for rebuilding service and checking status on each instance

---
+++ 033_aws-scaling/04_hands-on/01_challenge/tf/check.iron.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+for instance in $(./ssh.list.sh | jq -r '.n'); do
+    gcloud --project golang-web-dev-227919 compute ssh --zone europe-west2-a "$instance" -- systemctl status revolver
+done
