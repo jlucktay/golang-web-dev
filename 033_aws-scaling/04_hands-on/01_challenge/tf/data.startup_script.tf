1b657bc7e51aa2d1518dd383eaf839cfd2d96024
James Lucktaylor <jlucktay+github@gmail.com>
2019-01-19 18:47:33 +0000

    Finish wiring in private IP from GCP SQL DB master instance

--- 033_aws-scaling/04_hands-on/01_challenge/tf/data.startup_script.tf
+++ 033_aws-scaling/04_hands-on/01_challenge/tf/data.startup_script.tf
@@ -2,7 +2,15 @@ data "template_file" startup_script {
   template = "${file("${path.module}/gce.startup.sh")}"
 
   vars = {
-    mysql_ip       = ""
+    mysql_ip       = "${data.external.gcloud_sql.result.ipAddress}"
     mysql_password = "${module.sql.generated_user_password}"
   }
+}
+
+data "external" gcloud_sql {
+  program = [
+    "/usr/local/bin/bash",
+    "-c",
+    "${format("gcloud sql instances list --project golang-web-dev-227919 --format json | jq -r '.[].ipAddresses[] | select( .type == \"PRIVATE\" )'")}",
+  ]
 }
