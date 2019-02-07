f2b838306c711af2676d49088a2741e4e4f2d6c1
James Lucktaylor <jlucktay+github@gmail.com>
2019-02-07 13:32:57 +0000

    Complete 034/01

--- 034_photo-blog/01_starting/main.go
+++ 034_photo-blog/01_starting/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
+
+const cookieName = "my-session-uuid"
 
 var tpl *template.Template
 
@@ -13,10 +19,38 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/read", read)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	c := getCookie(w, req)
+	if err := tpl.ExecuteTemplate(w, "index.gohtml", c.Value); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie(cookieName)
+	if err != nil {
+		u := uuid.Must(uuid.NewV4())
+		c := &http.Cookie{
+			Name:  cookieName,
+			Value: u.String(),
+		}
+		http.SetCookie(w, c)
+	}
+
+	return c
+}
+
+func read(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie(cookieName)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "Your UUID cookie:", c)
 }
