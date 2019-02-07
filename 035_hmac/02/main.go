4ef0241f52fd423bb1971197787632b455f7d845
James Lucktaylor <jlucktay+github@gmail.com>
2019-02-07 14:16:39 +0000

    Add error handling

--- 035_hmac/02/main.go
+++ 035_hmac/02/main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -12,11 +13,10 @@ import (
 func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/authenticate", auth)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-
 	c, err := req.Cookie("session")
 	if err != nil {
 		c = &http.Cookie{
@@ -32,21 +32,22 @@ func foo(w http.ResponseWriter, req *http.Request) {
 
 	http.SetCookie(w, c)
 
-	io.WriteString(w, `<!DOCTYPE html>
-	<html>
-	  <body>
-	    <form method="POST">
-	      <input type="email" name="email">
-	      <input type="submit">
-	    </form>
-	    <a href="/authenticate">Validate This `+c.Value+`</a>
-	  </body>
-	</html>`)
+	if _, err := io.WriteString(w, `<!DOCTYPE html>
+<html>
+	<body>
+		<form method="POST">
+			<input type="email" name="email">
+			<input type="submit">
+		</form>
+		<a href="/authenticate">Validate This `+c.Value+`</a>
+	</body>
+</html>`); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
 func auth(w http.ResponseWriter, req *http.Request) {
-
 	c, err := req.Cookie("session")
 	if err != nil {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
@@ -71,17 +72,21 @@ func auth(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	io.WriteString(w, `<!DOCTYPE html>
-	<html>
-	  <body>
-	  	<h1>`+codeRcvd+` - RECEIVED </h1>
-	  	<h1>`+codeCheck+` - RECALCULATED </h1>
-	  </body>
-	</html>`)
+	if _, err := io.WriteString(w, `<!DOCTYPE html>
+<html>
+	<body>
+		<h1>`+codeRcvd+` - RECEIVED </h1>
+		<h1>`+codeCheck+` - RECALCULATED </h1>
+	</body>
+</html>`); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getCode(data string) string {
 	h := hmac.New(sha256.New, []byte("ourkey"))
-	io.WriteString(h, data)
+	if _, err := io.WriteString(h, data); err != nil {
+		log.Fatal(err)
+	}
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
