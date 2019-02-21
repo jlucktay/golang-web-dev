f706e1c3b4f4a17e978a651319da0d78343dd1c7
James Lucktaylor <jlucktay+github@gmail.com>
2019-02-21 12:18:49 +0000

    ...and also update main with new Mongo driver >_>

--- 042_mongodb/05_mongodb/05_update-user-controllers-delete/main.go
+++ 042_mongodb/05_mongodb/05_update-user-controllers-delete/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/jlucktay/golang-web-dev/042_mongodb/05_mongodb/05_update-user-controllers-delete/controllers"
 	"github.com/julienschmidt/httprouter"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/readpref"
 )
 
 func main() {
 	r := httprouter.New()
-	uc := controllers.NewUserController(getSession())
+	uc := controllers.NewUserController(getUserCollection())
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
-	http.ListenAndServe("localhost:8080", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
 
-func getSession() *mongo.Session {
-	s, err := mongo.Dial("mongodb://localhost")
+func getUserCollection() *mongo.Collection {
+	// Connect to our local mongo
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	client, errConnect := mongo.Connect(ctx, "mongodb://localhost:27017")
 
-	if err != nil {
-		panic(err)
+	// Check if connection error, is mongo running?
+	if errConnect != nil {
+		panic(errConnect)
 	}
-	return s
+
+	if errPing := client.Ping(ctx, readpref.Primary()); errPing != nil {
+		panic(errPing)
+	}
+
+	fmt.Println("Looks like we have successfully connected to MongoDB!")
+
+	collection := client.Database("042_mongodb").Collection("05_users")
+
+	return collection
 }
